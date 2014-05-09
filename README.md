9f93df3d71d0967fdf0a3355b8d0199bae58ea5d
soundslikebliss <russianid1@gmail.com>
2014-05-09 15:37:07 -0700

    first commit - resume site

---
+++ README.md
@@ -0,0 +1,9 @@
+### my resume...
+#### full-stack web development
+* Switchboard intern
+* Epicodus grad
+* rubyist
+* sinatra-lover
+* slim-user
+* NoSQL-curious
+* spring 2014
---
+++ app.rb
@@ -0,0 +1,24 @@
+class Me < Sinatra::Base
+  # require './idea'
+
+  not_found do
+    erb :error
+  end
+
+
+
+  get '/' do
+    slim :index
+  end
+
+  # post '/' do
+  # # 1. Create an idea based on the form parameters
+  # # idea = Idea.new
+  # idea = Idea.new(params['idea_title'], params['idea_description'])
+  # # 2. Store it
+  # idea.save
+  # # 3. Send us back to the index page to see all ideas
+  # redirect '/'
+  # end
+
+end
